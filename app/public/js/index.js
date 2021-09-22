7644d843ce1987b2c3c036c876e936fde72b7bf6
locdoannguyen <loc.nguyendoan.ntt@gmail.com>
2021-09-21 23:23:19 -0400

    Commit for hw3

---
+++ app/public/js/index.js
@@ -0,0 +1,51 @@
+const RandomPerson = {
+    data() {
+      return {
+        "Being": {
+
+        },
+        "firstname":"",
+        "lastname":"",
+        "country":"",
+        "age":"",
+        "email":"",
+        "birthday":"",
+        "pic":"",
+        "secondpic":""
+        
+      }
+    },
+    created() {
+        this.fetchUserData();
+    },
+
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.birthday).format('D MMM YYYY');
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then(responseJSON => {
+                var user = responseJSON.results[0];
+                this.firstname = user.name.first;
+                this.lastname = user.name.last;
+                this.country = user.location.country;
+                this.age = user.dob.age;
+                this.email = user.email;
+                this.birthday = user.dob.date;
+                this.pic = user.picture.large;
+                console.log(user);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    }
+    
+
+  }
+  
+  Vue.createApp(RandomPerson).mount('#rpApp');
